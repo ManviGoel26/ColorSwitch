aef4addf6b245e1ef2dc0b29abc167a704bf9b63
Jahnvi13 <55803815+Jahnvi13@users.noreply.github.com>
2020-12-08 23:17:26 +0530

    Add files via upload

---
+++ ColorSwitch/src/gameplay/Obstacle.java
@@ -0,0 +1,36 @@
+package burj;
+
+import java.util.ArrayList;
+
+import javafx.scene.Group;
+
+public class Obstacle{
+	
+	//public ArrayList<Shape> nodes;
+	public ArrayList<ObstacleSprite> obsprites;	//on screen obstacles
+	
+	public Obstacle(){
+		
+		obsprites=new ArrayList<>();
+		obsprites.add(new ObstacleSprite());
+		//getChildren().add(rectangle);
+	}
+	
+	public boolean detectCollision() {
+		
+		for(ObstacleSprite ob: obsprites) {
+			if(ob.checkIfCollided())
+				return true;
+		}
+		return false;
+		
+	}
+	
+	public void setTranslations(Group root) {
+		
+		for(ObstacleSprite ob: obsprites) {
+			ob.setTranslation(root);
+		}
+		
+	}
+}
---
+++ ColorSwitch/src/gameplay/ObstacleSprite.java
@@ -0,0 +1,75 @@
+package burj;
+
+import java.util.ArrayList;
+
+import javafx.animation.Animation;
+import javafx.animation.TranslateTransition;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
+import javafx.util.Duration;
+
+public class ObstacleSprite {
+	
+	public ArrayList<Shape> nodes;
+	boolean onscreen;
+	
+	public ObstacleSprite() {
+		int i=1; //random number
+		switch(i) {
+			case 1: CircleSprite();
+				break;
+				
+		}
+	}
+	public ObstacleSprite CircleSprite(){
+		
+		nodes = new ArrayList<>();
+		
+		Circle r=new Circle(40, 30, 30);
+		r.setFill(Color.RED);
+		
+		Circle g=new Circle(100, 30, 30);
+		g.setFill(Color.GREEN);
+		
+		Circle b=new Circle(160, 30, 30);
+		b.setFill(Color.BLUE);
+		
+		Circle y=new Circle(220, 30, 30);
+		y.setFill(Color.YELLOW);
+		
+	    nodes.add(r);
+	    nodes.add(g);
+	    nodes.add(b);
+	    nodes.add(y);
+	    
+	    return this;
+	}
+	public boolean checkIfCollided() {
+		
+		System.out.println("checked");
+		boolean collisionDetected = false;
+		  for (Shape static_bloc : nodes) {
+		    Shape intersect = Shape.intersect(User.rectangle, static_bloc);
+		      if (intersect.getBoundsInLocal().getWidth() != -1 && User.rectangle.getFill()!=static_bloc.getFill()) {
+		        collisionDetected = true;
+		      }
+		    
+		  }
+		return collisionDetected;
+	}
+	public void setTranslation(Group root) {
+		for(Shape i: nodes) {
+			TranslateTransition transition=new TranslateTransition();
+	    	transition.setDuration(Duration.seconds(3));
+	    	transition.setToX(500);
+	    	transition.setAutoReverse(true);
+	    	transition.setCycleCount(Animation.INDEFINITE);
+	    	transition.setNode(i);
+	    	transition.play();
+	    }
+		root.getChildren().addAll(nodes);
+	}
+	
+}
