ba6c791d3d3e37711f66bb8783f4ee19f0e41563
Jahnvi13 <55803815+Jahnvi13@users.noreply.github.com>
2020-12-13 21:27:35 +0530

    Update Obstacle.java

--- ColorSwitch/src/gameplay/Obstacle.java
+++ ColorSwitch/src/gameplay/Obstacle.java
@@ -6,14 +6,19 @@ import javafx.scene.Group;
 
 public class Obstacle{
 	
-	//public ArrayList<Shape> nodes;
 	public ArrayList<ObstacleSprite> obsprites;	//on screen obstacles
 	
 	public Obstacle(){
 		
 		obsprites=new ArrayList<>();
-		obsprites.add(new ObstacleSprite());
-		//getChildren().add(rectangle);
+		
+		int x=250;
+		for(int i=0;i<10;i++) {
+			obsprites.add(new ObstacleSprite(i%3+1, x));
+			x-=400;
+		}
+
+		
 	}
 	
 	public boolean detectCollision() {
@@ -28,9 +33,21 @@ public class Obstacle{
 	
 	public void setTranslations(Group root) {
 		
-		for(ObstacleSprite ob: obsprites) {
-			ob.setTranslation(root);
+		for(int i=0;i<10;i++) {
+			if(i%3==0)
+				obsprites.get(i).setTranslation2(root,3);
+			else if(i%3==1) 
+				obsprites.get(i).setTranslation(root,3);
+			else
+				obsprites.get(i).setTranslation2(root,3);
 		}
 		
+		
+	}
+	public void update() {//Group root) {
+		for(ObstacleSprite ob: obsprites) {
+			ob.moveDown();//sroot);
+		}
 	}
 }
+
