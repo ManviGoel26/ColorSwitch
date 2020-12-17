760c2392b9949e193a75e5fac824cbd50afbf1ae
Jahnvi13 <55803815+Jahnvi13@users.noreply.github.com>
2020-12-17 16:01:55 +0530

    Update Obstacle.java

--- ColorSwitch/src/gameplay/Obstacle.java
+++ ColorSwitch/src/gameplay/Obstacle.java
@@ -1,79 +1,79 @@
 package gameplay;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
-
+import java.util.Random;
+import java.io.IOException;	
+import java.io.ObjectOutputStream;	
+import java.io.Serializable;
 import javafx.scene.Group;
 
-public class Obstacle implements Serializable
-{
-	
+public class Obstacle implements Serializable{
 	public ArrayList<ObstacleSprite> obsprites;	//on screen obstacles
 	
-	public Obstacle()
-	{
-		obsprites = new ArrayList<>();
+	public Obstacle(int startht){
 		
-		int x = 250;
-		for(int i = 0; i < 10; i++) 
-		{
-			obsprites.add(new ObstacleSprite(i%3+1, x));
-			x -= 400;
+		obsprites=new ArrayList<>();
+		
+		int x=400;
+		int noOfTypes=5;
+		Random random=new Random();
+		for(int i=0;i<20;i++) {
+			Random r=new Random();
+			int t=Math.abs(r.nextInt());
+			//System.out.println(t%noOfTypes+1);
+			obsprites.add(new ObstacleSprite(t%noOfTypes+1, startht));
+			startht-=x;
 		}
 	}
 	
-	public boolean detectCollision() 
-	{
+	public boolean detectCollision() {
+		
 		for(ObstacleSprite ob: obsprites) {
 			if(ob.checkIfCollided())
 				return true;
 		}
-		return false;	
+		return false;
 	}
 	
-	public void setTranslations(Group root) 
-	{	
-		for(int i = 0; i < 10; i++) 
-		{
-			if(i%3 == 0)
-			{
-				obsprites.get(i).setTranslation2(root,3);
-			}
+	public void setTranslations(Group root) {
+		int nooftypes=5;
+		for(int i=0;i<15;i++) {
+			int shapetype=obsprites.get(i).Shape_number;
 			
-			else if(i%3 == 1) 
-			{
+			if(shapetype==0)
+				obsprites.get(i).setTranslation2(root,3);
+			else if(shapetype==1) 
 				obsprites.get(i).setTranslation(root,3);
-			}
-			
-			else
-			{
+			else if(shapetype==2)
 				obsprites.get(i).setTranslation2(root,3);
-			}
+			else if(shapetype==3)
+				obsprites.get(i).setTranslation3(root,3);
+			else if(shapetype==4)
+				obsprites.get(i).setTranslation4(root,3);
+			
 		}
 		
 		
 	}
-	public void update()
-	{
-		for(ObstacleSprite ob: obsprites) 
-		{
+	public void update() {//Group root) {
+		for(ObstacleSprite ob: obsprites) {
 			ob.moveDown();//sroot);
 		}
 	}
-	
-	private void writeObject(ObjectOutputStream output) throws IOException
-	{
+	private void writeObject(ObjectOutputStream output) throws IOException	
+	{	
 		output.defaultWriteObject();
 		
-//		System.out.println("wdrine");
-		
-
+	}
+// 		public void renew(int startht) {
+// 		obsprites=new ArrayList<>();
+				
+// 		int x=400;
+// 		int noOfTypes=5;
+// 		for(int i=0;i<15;i++) {
+// 			obsprites.add(new ObstacleSprite(i%noOfTypes+1, startht));
+// 			startht-=x;
+// 		}
 		
 	}
-	
-
-
 }
-
